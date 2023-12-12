234e19713cd8a50486bcb7eb030497352901f93b
Kay56 <hegyi.gergely1@gmail.com>
2023-12-12 13:26:03 +0100

    Endpointok hozzáadása

--- server.js
+++ server.js
@@ -1,16 +1,54 @@
 require('dotenv').config();
 const express = require('express');
+var mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
-
-var db_modul = require('./modules/database');
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json());
 
-app.use('/db', db_modul);
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME,
+    timezone: 'UTC'
+  });
+
+app.get('/', function (req, res) {
+    res.send('Simpe NodeJS Backend API');
+});
+
+app.get('/:table', (req, res) => {
+    pool.query(`SELECT * FROM zarodolgozat`, (err, results) => {
+        sendResults(table, err, results, req, res, 'sent from');
+    });
+});
+
+app.get('/:table/:id', (req, res) => {
+    let id = req.params.id;
+    
+    pool.query(`SELECT * FROM zarodolgozat WHERE ID=${id}`, (err, results) => {
+      sendResults(table, err, results, req, res, 'sent from');
+    });
+  });
+
+  app.get('/:table/:field/:op/:value', (req, res)=>{
+    let field = req.params.field;
+    let value = req.params.value;
+    let op = getOperator(req.params.op);
+    
+    if (op == ' like '){
+      value = `%${value}%`;
+    }
+  
+    pool.query(`SELECT * FROM zarodolgozat WHERE ${field}${op}'${value}'`, (err, results)=>{
+      sendResults(table, err, results, req, res, 'sent from');
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
